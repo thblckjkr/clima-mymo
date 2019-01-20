945f6fb73c835c9fd0a1d44e89763eaf860f954c
Teo Gonzalez <teo.gonzalez.c@hotmail.com>
2019-01-19 23:55:17 -0700

    Move passwords to a separate file

--- mymo.js
+++ mymo.js
@@ -1,12 +1,17 @@
 var ZongJi = require('zongji');
 var Parser = require('./lib/parser');
 
+const db = require('./database/config.json');
+
+// Link and instanciate the parser for storing the data
 var parser = new Parser();
 
+// MySQL credentials [Change here]
 var zongji = new ZongJi({
-   host: "localhost",
-   user: "maxwell",
-   password: "password"
+   serverId : db.mysql.serverId,
+   host: db.mysql.host,
+   user: db.mysql.username,
+   password: db.mysql.password
 });
 
 // Each change to the replication log results in an event
@@ -17,6 +22,7 @@ zongji.on('binlog', function(evt) {
 });
 
 // Binlog must be started, optionally pass in filters
+// tablemap is important to catch all the other events
 zongji.start({
   includeEvents: ['tablemap', 'writerows', 'updaterows', 'deleterows']
 });
