7df301e5bdd2a8007888403fec856793ea35af86
Teo Gonzalez Calzada <teo.gonzalez.c@hotmail.com>
2019-05-21 16:57:25 -0600

    Creation of importer

---
+++ importer/import.py
@@ -0,0 +1,42 @@
+# simple information importer
+import json
+import MySQLdb
+import requests
+
+# xml requests
+from xml.dom import minidom
+
+class uploader:
+   def __init__(self):
+      # Get xml from uri
+      r = requests.get(config['schema']['url'], allow_redirects=False)
+      xmldoc = minidom.parse(r.content)
+      xmldoc.getElementsByTagName('Estacion')
+      
+
+
+with open('../config/config.json') as json_file:
+   config = json.load(json_file)
+
+conn = MySQLdb.connect(
+   host = config['mysql']['host'],
+   user = config['mysql']['username'],
+   passwd = config['mysql']['username']
+)
+
+temp = raw_input("Que base de datos desea importar?")
+
+cursor = conn.cursor()
+cursos.execute('use ' + database)
+cursor.execute('select * from archive')
+
+
+
+# Obtener una a una toda la informacion
+while True:
+   row = cursor.fetchone()
+   if row == None:
+      break
+   print(row)
+
+db.close()
